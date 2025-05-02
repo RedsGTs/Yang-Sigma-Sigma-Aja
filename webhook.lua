16d837e56e77e83db5797bb65a39b3c18f7c2c3c
RedsGTs <116986655+RedsGTs@users.noreply.github.com>
2025-05-02 17:41:24 +0700

    Update webhook.lua

--- webhook.lua
+++ webhook.lua
@@ -1,28 +1,28 @@
-local HttpService = game:GetService("HttpService")
-local Webhook_URL = "https://discord.com/api/webhooks/1111606979585130537/0joXFyaI312c33vvQLZ0-7M7dCOJJjIeRYQVxB2qyMg79N0ZSZokugMrbI9G9WhoOnHl"
+HttpService = game:GetService("HttpService")
+Webhook_URL = "https://discord.com/api/webhooks/1111606979585130537/0joXFyaI312c33vvQLZ0-7M7dCOJJjIeRYQVxB2qyMg79N0ZSZokugMrbI9G9WhoOnHl"
 
-local response = syn.request({
+local responce=syn request(
+{
     Url = Webhook_URL,
-    Method = "POST",
+    Method = 'POST',
     Headers = {
-        ["Content-Type"] = "application/json"
+        ['Content-Type'] = 'application/json'
     },
     Body = HttpService:JSONEncode({
-        content = "",
-        embeds = {
-            {
-                title = "**Ambanutt**",
-                description = game.Players.LocalPlayer.DisplayName .. " has executed the script.",
-                type = "rich",
-                color = tonumber(0xffffff),
-                fields = {
-                    {
-                        name = "Hardware ID:",
-                        value = game:GetService("RbxAnalyticsService"):GetClientId(),
-                        inline = true
-                    }
+        ["content"] = "",
+        ["embeds"] = ({
+            ["title"] = "**Ambanutt**",
+            ["description"] = game.Players.LocalPlayer.DisplayName.."has executed the script.",
+            ["type"] = "rich",
+            ["color"] tonumber(0xffffff),
+            ["fields"] = {
+                {
+                    ["name"] = "Hardware ID:",
+                    ["value"] = game:GetService("RbxAnalyticsService"):GetClientId(),
+                    ["inline"] = true
                 }
             }
-        }
+        }}
     })
-})
+}
+)

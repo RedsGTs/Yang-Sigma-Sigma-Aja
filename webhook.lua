59488328e827fd727809b0597658bd82f1b4ea90
RedsGTs <116986655+RedsGTs@users.noreply.github.com>
2025-05-02 16:28:18 +0700

    Update webhook.lua

--- webhook.lua
+++ webhook.lua
@@ -1,29 +1,24 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local HttpService = game:GetService("HttpService")
 
-local webhookUrl = "https://discord.com/api/webhooks/1111606979585130537/0joXFyaI312c33vvQLZ0-7M7dCOJJjIeRYQVxB2qyMg79N0ZSZokugMrbI9G9WhoOnHl"
-local sendWebhook = ReplicatedStorage:WaitForChild("SendWebhook")
+local url = "https://discord.com/api/webhooks/1111606979585130537/0joXFyaI312c33vvQLZ0-7M7dCOJJjIeRYQVxB2qyMg79N0ZSZokugMrbI9G9WhoOnHl"
 
-sendWebhook.OnServerEvent:Connect(function(player)
-    local data = {
-        ["content"] = "**" .. player.Name .. "** executed the script in the game!"
-    }
+local data = {
+    content = "Player " .. game.Players.LocalPlayer.Name .. " executed a script using Delta!"
+}
 
-    local headers = {
-        ["Content-Type"] = "application/json"
-    }
-
-    local success, response = pcall(function()
-        HttpService:PostAsync(
-            webhookUrl,
-            HttpService:JSONEncode(data),
-            Enum.HttpContentType.ApplicationJson
-        )
-    end)
-
-    if success then
-        print("Webhook sent for", player.Name)
-    else
-        warn("Webhook failed:", response)
-    end
+local success, err = pcall(function()
+    http_request({
+        Url = url,
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json"
+        },
+        Body = HttpService:JSONEncode(data)
+    })
 end)
+
+if success then
+    print("Webhook sent.")
+else
+    warn("Failed to send webhook:", err)
+end

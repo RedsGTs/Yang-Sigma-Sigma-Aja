eb83ace990c5b6e19c3920d1e30d8ae4c25d5f17
RedsGTs <116986655+RedsGTs@users.noreply.github.com>
2025-05-02 15:28:37 +0700

    Update Afk.lua

--- Afk.lua
+++ Afk.lua
@@ -1,10 +1,20 @@
--- Anti AFK Script for Roblox
+-- Anti AFK Script for Roblox with Notification
 -- Place this LocalScript in StarterPlayerScripts to prevent auto kick due to inactivity
 
 local Players = game:GetService("Players")
 local VirtualUser = game:GetService("VirtualUser")
+local StarterGui = game:GetService("StarterGui")
 
 local player = Players.LocalPlayer
+
+-- Function to send notification
+local function sendNotification()
+    StarterGui:SetCore("SendNotification", {
+        Title = "Anti AFK";
+        Text = "Anti AFK activated to prevent kick.";
+        Duration = 3;
+    })
+end
 
 -- Connect to the Idled event to simulate user input when the player is idle
 player.Idled:Connect(function()
@@ -12,6 +22,8 @@ player.Idled:Connect(function()
     VirtualUser:CaptureController()
     -- Simulate button2 down and up (right mouse button click)
     VirtualUser:ClickButton2(Vector2.new())
+    -- Show notification
+    sendNotification()
 end)
 
 -- Optional: You can add a loop to simulate other input periodically as an extra precaution
@@ -20,6 +32,6 @@ while true do
     wait(60) -- every 60 seconds
     VirtualUser:CaptureController()
     VirtualUser:ClickButton2(Vector2.new())
+    sendNotification()
 end
 --]]
-
